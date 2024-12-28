e8ef39ffe91836378f6dfb79674fbc2638fa3d21
AQSA-ABDULLAH <aqsaabdullah38403@gmail.com>
2024-12-28 09:03:24 +0500

    complete header section

--- components/header/Header.jsx
+++ components/header/Header.jsx
@@ -2,23 +2,30 @@ import React from "react";
 
 export default function Header() {
   return (
-    <header className="flex justify-between items-center text-white p-6">
-      <div className="flex items-center gap-6">
-        <img src="/assets/Group 3055.png" alt="Zima Logo" className="h-8" />
-        <img src="/assets/Group 3385.png" alt="Zima Logo" className="h-8" />
-      </div>
-      <div>
-        <img src="/assets/Group 3005.png" />
-      </div>
-      <div className="flex items-center gap-4">
-        <a href="#login" className="hidden md:block hover:underline">
-          Just For You
-        </a>
-        <img src="/assets/Group 3035.png" alt="UK Flag" className="h-6" />
-        <div className="flex gap-8">
-          <img src="/assets/Path 7680.png" alt="glob" className="h-6" />
-          <img src="/assets/Path 7582.png" alt="bag" className="h-6" />
-          <img src="/assets/Group 2867.png" alt="user" className="h-6" />
+    <header className="flex justify-between items-center text-white p-8">
+      <div className="flex w-full">
+        {/* First Div */}
+        <div className="flex items-center gap-6 flex-1">
+          <img src="/assets/Group 3055.png" alt="Zima Logo" />
+          <img src="/assets/Group 3385.png" alt="Zima Logo" />
+        </div>
+
+        {/* Second Div */}
+        <div className="flex justify-center flex-1">
+          <img src="/assets/Group 3005.png" alt="Center Logo" />
+        </div>
+
+        {/* Third Div */}
+        <div className="flex items-center gap-6 justify-end flex-1">
+          <div className="flex gap-8">
+            <p className="text-[20px] tracking-wide">JUST FOR YOU</p>
+            <img src="/assets/Group 3035.png" alt="UK Flag" />
+          </div>
+          <div className="flex gap-12">
+            <img src="/assets/Path 7680.png" alt="glob" />
+            <img src="/assets/Path 7582.png" alt="bag" />
+            <img src="/assets/Group 2867.png" alt="user" />
+          </div>
         </div>
       </div>
     </header>
